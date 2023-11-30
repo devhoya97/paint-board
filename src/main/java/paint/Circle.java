378964b6e697a6400a7f820c933b983acbebf5fd
devhoya97 <diajbh123@gmail.com>
2023-11-30 16:13:38 +0900

    fix: 좌표체계 변경으로 인한 원의 중심과 반지름 검증기능 수정

--- src/main/java/paint/Circle.java
+++ src/main/java/paint/Circle.java
@@ -2,9 +2,9 @@ package paint;
 
 public class Circle {
     public static final int MIN_X = 1;
-    public static final int MAX_X = 78;
+    public static final int MAX_X = 79;
     public static final int MIN_Y = 1;
-    public static final int MAX_Y = 28;
+    public static final int MAX_Y = 29;
     private final Point origin;
     private final int radius;
 
@@ -15,8 +15,10 @@ public class Circle {
     }
 
     private void validate(Point origin, int radius) {
-        if ((origin.getX() - radius < MIN_X) || (origin.getX() + radius > MAX_X)
-                || (origin.getY() - radius < MIN_Y || origin.getY() + radius > MAX_Y)) {
+        if (((origin.getX() - radius) >= MIN_X)
+                || ((origin.getX() + radius) <= MAX_X)
+                || ((origin.getY() - radius) >= MIN_Y)
+                || ((origin.getY() + radius) <= MAX_Y)) {
             throw new IllegalArgumentException();
         }
     }
